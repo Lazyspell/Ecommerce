95c7d374cb2b624eb5c5859ca493c0da25435074
Jeremy Elam <lazyspell@Jeremys-MacBook-Pro.local>
2022-04-26 15:13:11 -0700

    added sytling to the navigation bar

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,16 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
 
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.styles.scss";
+
 function Navigation() {
     return (
         <Fragment>
-            <div className="Navigation">
+            <div className="navigation">
                 <Link className="logo-container" to="/">
-                    <div>Logo</div>
+                    <CrwnLogo className="logo" />
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to="/shop">
