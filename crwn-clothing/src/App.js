ef621d03d3a2388a087ed51323132e6d63e8975c
Jeremy Elam <lazyspell@Jeremys-MacBook-Pro.local>
2022-04-26 14:12:20 -0700

    basic navigation bar set up created

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,11 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import "./categories.styles.scss";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
 function App() {
     return (
         <Routes>
-            <Route path="/home" element={<Home />}></Route>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+                <Route path="/shop" />
+            </Route>
         </Routes>
     );
 }
