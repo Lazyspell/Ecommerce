52299d9c83dc1a8c2641e33d48ca43398d97af5d
Jeremy Elam <lazyspell@Jeremys-MacBook-Pro.local>
2022-04-26 09:44:21 -0700

    added routing the react application

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,17 +1,14 @@
-import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./categories.styles.scss";
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
 
 function App() {
-    const [categories, setCategories] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:8080/categories")
-            .then((response) => response.json())
-            .then((items) => setCategories(items));
-    }, []);
-
-    return <Directory data={categories} />;
+    return (
+        <Routes>
+            <Route path="/home" element={<Home />}></Route>
+        </Routes>
+    );
 }
 
 export default App;
---
+++ crwn-clothing/src/routes/home/home.component.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import "../../categories.styles.scss";
+
+import Directory from "../../components/directory/directory.component";
+
+function Home() {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/categories")
+            .then((response) => response.json())
+            .then((items) => setCategories(items));
+    }, []);
+
+    return <Directory data={categories} />;
+}
+
+export default Home;
