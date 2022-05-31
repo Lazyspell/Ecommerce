3598a361eb892e608302d6864618c304b2a7ca9b
Lazyspell <jeremy2975@yahoo.com>
2022-05-30 19:15:53 -0700

    added store icon and store drop down menu to navigation bar

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -2,10 +2,13 @@ import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import { UserContext } from "../../contexts/user.context";
 import { logOutUser } from "../../remote/user";
 
 import "./navigation.styles.scss";
+
 function Navigation() {
     const { currentUser, setCurrentUser } = useContext(UserContext);
     const signOutHandler = async () => {
@@ -31,7 +34,9 @@ function Navigation() {
                             Sign In
                         </Link>
                     )}
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
