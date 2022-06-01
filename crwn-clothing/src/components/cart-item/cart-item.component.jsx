599cf349fde884e9a9399abd87a11f81640ff927
Lazyspell <jeremy2975@yahoo.com>
2022-05-31 21:48:36 -0700

    added styling to cart drop down and keeping count of total in cart icon

--- crwn-clothing/src/components/cart-item/cart-item.component.jsx
+++ crwn-clothing/src/components/cart-item/cart-item.component.jsx
@@ -1,11 +1,16 @@
 import "./cart-item.styles.scss";
 
 const CartItem = ({ cartItem }) => {
-    const { product_name, quantity } = cartItem;
+    const { product_name, quantity, image_url, price } = cartItem;
     return (
-        <div>
-            <h2>{product_name}</h2>
-            <span>{quantity}</span>
+        <div className="cart-item-container">
+            <img src={image_url} alt={`${product_name}`} />
+            <div className="item-details">
+                <span className="name">{product_name}</span>
+                <span className="price">
+                    {quantity} x ${price}
+                </span>
+            </div>
         </div>
     );
 };
