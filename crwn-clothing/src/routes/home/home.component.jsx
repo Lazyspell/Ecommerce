250ed45ba5efedbc7c462978c1a8b2ba7e5adc83
Lazyspell <jeremy2975@yahoo.com>
2022-06-06 11:49:01 -0700

    removed fetch and used the remote created for categories

--- crwn-clothing/src/routes/home/home.component.jsx
+++ crwn-clothing/src/routes/home/home.component.jsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from "react";
 import "../../categories.styles.scss";
 
 import Directory from "../../components/directory/directory.component";
+import { allCategories } from "../../remote/categories";
 
 const Home = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:8080/categories")
-            .then((response) => response.json())
-            .then((items) => setCategories(items));
+        const getCategories = async () => {
+            const category = await allCategories();
+            setCategories(category.data);
+        };
+        getCategories();
     }, []);
 
     return <Directory data={categories} />;
