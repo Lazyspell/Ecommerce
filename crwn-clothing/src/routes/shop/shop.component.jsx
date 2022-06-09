14f6744866ec2ff79caa12e6ab09ac990109235b
lazyspell <Jeremy2975@yahoo.com>
2022-06-08 21:55:41 -0700

    change functionality so that it orginizes the displays based off product category

--- crwn-clothing/src/routes/shop/shop.component.jsx
+++ crwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import ProductCard from "../../components/product-card/product-card.component";
 import { ProductContext } from "../../contexts/products.context";
 
@@ -6,13 +6,20 @@ import "./shop.styles.scss";
 
 const Shop = () => {
     const { products } = useContext(ProductContext);
-
+    console.log(products);
     return (
-        <div className="products-container">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
+        <Fragment>
+            {Object.keys(products).map((title) => (
+                <Fragment key={title}>
+                    <h2>{title}</h2>
+                    <div className="products-container">
+                        {products[title].map((items) => (
+                            <ProductCard key={items.id} product={items} />
+                        ))}
+                    </div>
+                </Fragment>
             ))}
-        </div>
+        </Fragment>
     );
 };
 
