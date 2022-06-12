50e98b35d21ca2ce2897fa52803194b66a96bfb0
lazyspell <Jeremy2975@yahoo.com>
2022-06-12 12:37:06 -0700

    change styles in directory component from scss to jsx

--- crwn-clothing/src/components/directory-item/directory-item.component.jsx
+++ crwn-clothing/src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,20 @@
-import "./directory-item.styles.scss";
+import "./directory-item.styles.jsx";
+import {
+    BackgroundImage,
+    Body,
+    DirectoryItemContainer,
+} from "./directory-item.styles.jsx";
 
 const DirectoryItem = ({ data }) => {
     const { image_url, category } = data;
     return (
-        <div className="directory-item-container">
-            <div
-                className="background-image"
-                style={{ backgroundImage: `url(${image_url})` }}
-            />
-            <div className="body">
+        <DirectoryItemContainer>
+            <BackgroundImage style={{ backgroundImage: `url(${image_url})` }} />
+            <Body>
                 <h2>{category}</h2>
                 <p>Shop Now</p>
-            </div>
-        </div>
+            </Body>
+        </DirectoryItemContainer>
     );
 };
 
